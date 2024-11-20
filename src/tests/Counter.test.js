854345dcdf74d8a8e4407d2ec3f1dcae73573408
kamilo408 <jrodriguez21@ufl.edu>
2024-11-20 14:41:50 -0500

    Test unit for counter.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -3,30 +3,35 @@ import { render, screen, fireEvent } from '@testing-library/react';
 // Import counter.js 
 import Counter from '../components/Counter';
 
-
-
-test('renders counter message', () => {
+beforeEach(() => {
   render(<Counter />);
+  
+})
+
+
+// test the counter message
+test('renders counter message', () => {
+  
   const heading = screen.getByText(/counter/i);
   expect(heading).toBeInTheDocument();
 });
-
+//test the value starts with 0 
 test('should render initial count with value of 0', () => {
-  render(<Counter />);
+  
   const count = screen.getByTestId('count');
   expect(count).toHaveTextContent('0');
 });
-
+// test if count incremetnts by clicling +
 test('clicking + increments the count', () => {
-  render(<Counter />);
+  
   const incrementButton = screen.getByText('+');
   fireEvent.click(incrementButton);
   const count = screen.getByTestId('count');
   expect(count).toHaveTextContent('1');
 });
-
+//test if count decrements by clicling -
 test('clicking - decrements the count', () => {
-  render(<Counter />);
+
   const decrementButton = screen.getByText('-');
   fireEvent.click(decrementButton);
   const count = screen.getByTestId('count');
